a1c6fb0d354ad2686e8db388a10f0ec92be86ce6
Utkarsh Raj <utkarsh436@gmail.com>
2023-01-16 18:35:50 -0500

    set up association table between users and their loans

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def get_user(user_id: int):
             "status": 404
         }
     db_session = SessionLocal()
-    result = db_session.query(models.User).filter(models.User.id == user_id).first()
+    result = db_session.query(models.UserModel).filter(models.UserModel.id == user_id).first()
     if not result:
         return {
             "message": "no user found",
@@ -48,10 +48,11 @@ async def create_user(request: Request):
     first_name = payload["first_name"]
     last_name = payload["last_name"]
 
-    user_model = models.User(email=email, hashed_password=password, first_name=first_name, last_name=last_name)
+    user_model = models.UserModel(email=email, hashed_password=password, first_name=first_name, last_name=last_name)
     db_session.add(user_model)
     db_session.commit()
     db_session.refresh(user_model)
+    db_session.close()
 
     return {
         "data": user_model
@@ -80,12 +81,22 @@ async def create_loan(request: Request):
         user_ids = user_detail["user_ids"]
         owner = user_detail["owner"]
 
+        db_session = SessionLocal()
+        loan_model = models.LoanModel(amount=loan_amount, interest=loan_interest, term_months=loan_months, owner=owner)
+        db_session.add(loan_model)
+        db_session.commit()
+        db_session.refresh(loan_model)
 
+        for id in user_ids:
+            user_obj = db_session.query(models.UserModel).filter(models.UserModel.id == id).first()
+            if user_obj:
+                user_obj.loans.append(loan_model)
+                db_session.commit()
 
         return {
             "message": "loan created",
             "data": {
-                "payload": payload
+                "loan_id": loan_model.id
             },
             "status": 200
         }
@@ -94,11 +105,49 @@ async def create_loan(request: Request):
 
 @app.get("/loans/{loan_id}")
 def get_loan(loan_id: int):
-    return{}
+    if not loan_id:
+        return {
+            "message": "no loan id passed",
+            "data": {},
+            "status": 404
+        }
+    db_session = SessionLocal()
+    result = db_session.query(models.LoanModel).filter(models.LoanModel.id == loan_id).first()
+    if not result:
+        return {
+            "message": "no loan found",
+            "data": {},
+            "status": 404
+        }
+    return {
+        "data": result
+    }
 
-@app.get("/loans/{user_id}")
+@app.get("/loans/user/{user_id}")
 def get_user_loans(user_id: int):
-    return{}
+    """
+    gets all loan objects for a user
+    :param user_id:
+    :return: array of loan objects associated to user
+    """
+    if not user_id:
+        return {
+            "message": "no user id passed",
+            "data": {},
+            "status": 404
+        }
+    db_session = SessionLocal()
+    try:
+        user_obj = db_session.query(models.UserModel).filter(models.UserModel.id == user_id).first()
+
+        res_arr = user_obj.loans
+        return {
+            "loans": res_arr
+        }
+    except Exception as e:
+        return {e}
+
+
 
 @app.get("/loans/summary/{loan_id}")
 def get_loan_summary(loan_id: int):
--- models.py
+++ models.py
@@ -1,31 +1,34 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Table, Float
 from sqlalchemy.orm import relationship
 
 from database import Base
 
 
-class User(Base):
+association_table = Table(
+    'association',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey("users.id")),
+    Column('loan_id', Integer, ForeignKey("loans.id"))
+)
+
+class UserModel(Base):
     __tablename__ = "users"
 
+    id = Column('id', Integer, primary_key=True, index=True, unique=True)
+    email = Column('email', String, unique=True, index=True)
+    hashed_password = Column('hashed_password', String)
+    first_name = Column('first_name', String)
+    last_name = Column('last_name', String)
+
+    loans = relationship('LoanModel', secondary=association_table, back_populates='users')
+
+class LoanModel(Base):
+    __tablename__ = "loans"
+
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    first_name = Column(String)
-    last_name = Column(String)
+    amount = Column(String)
+    term_months = Column(String)
+    owner = Column(Integer)
+    interest = Column(Float)
 
-class Loan(Base):
-    __tablename__ = "loan"
-
-    id = Column(Integer, primary_key=True, index=True)
-    amount = Column(String, unique=True, index=True)
-    loan_terms_months = Column(String)
-
-# class user_loans(Base):
-#     __tablename__ = "user_loans"
-#
-#     id = Column(Integer, primary_key=True, index=True)
-#     owner = Column(Integer)
-#     user_id = Column(Integer, ForeignKey("users.id"))
-#
-#
-#     # loan_id = relationship()
+    users = relationship('UserModel', secondary=association_table, back_populates='loans')
