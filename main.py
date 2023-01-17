724934828c2cd8abc7a8698757c314062f3e6f0f
utkarshraj <utkarsh436@gmail.com>
2023-01-17 17:32:55 -0500

    added validation for get_user, create_user, and create_loan

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import re
 
 import models
 from fastapi import FastAPI, Request
@@ -7,15 +8,21 @@ from database import engine, SessionLocal
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+
 @app.get("/")
 def homepage():
-    return{
+    return {
         "Welcome to loan amortization app"
     }
 
 
 @app.get("/user/{user_id}")
 def get_user(user_id: int):
+    """
+    Gets the user information for a specific user based on the id
+    :param user_id:
+    :return: user object
+    """
     if not user_id:
         return {
             "message": "no user id passed",
@@ -32,7 +39,9 @@ def get_user(user_id: int):
                 "status": 404
             }
         return {
-            "data": result
+            "message": "user found",
+            "data": {result},
+            "status": 200
         }
     except Exception as e:
         return {e}
@@ -42,20 +51,56 @@ def get_user(user_id: int):
 
 @app.post("/user")
 async def create_user(request: Request):
+    """
+    creates a user
+    Required an email, password, first_name, last_name
+    Sample payload:
+    {
+        "email": "foo@gmail.com",
+        "first_name": "foo",
+        "last_name": "bar",
+        "password": "password"
+    }
+    :param request:
+    :return: {
+            "data": {
+                "first_name": "foo",
+                "email": "foo@gmail.com",
+                "hashed_password": "password",
+                "id": 1,
+                "last_name": "bar"
+            }
+        }
+    """
     payload = await request.json()
     db_session = SessionLocal()
     if not payload:
-        return{
+        return {
             "message": "invalid payload",
             "data": {},
             "status": 404
         }
     try:
+        email = payload.get("email")
+        password = payload.get("password")
+        first_name = payload.get("first_name")
+        last_name = payload.get("last_name")
 
-        email = payload["email"]
-        password = payload["password"]
-        first_name = payload["first_name"]
-        last_name = payload["last_name"]
+        if not email or not password or not first_name or not last_name:
+            raise Exception("missing parameters")
+        if not check_email(email):
+            raise Exception("invalid email address")
+
+        # check if user already exists if so then return user object
+        user_obj = db_session.query(models.UserModel).filter(models.UserModel.email == email).first()
+        if user_obj:
+            return {
+                "message": "user already exists",
+                "data": {
+                    user_obj
+                },
+                "status": 400
+            }
 
         user_model = models.UserModel(email=email, hashed_password=password, first_name=first_name, last_name=last_name)
         db_session.add(user_model)
@@ -66,7 +111,11 @@ async def create_user(request: Request):
             "data": user_model
         }
     except Exception as e:
-        return{e}
+        return {
+            "message": str(e),
+            "data": {},
+            "status": 400
+        }
     finally:
         db_session.close()
 
@@ -77,23 +126,32 @@ async def create_loan(request: Request):
     db_session = SessionLocal()
     try:
         payload = await request.json()
-        if not payload or not payload["loan_detail"] or not payload["user_detail"]:
+        if not payload or not payload.get("loan_detail") or not payload.get("user_detail"):
             return {
                 "message": error_message,
                 "data": {},
                 "status": 404
             }
 
-        loan_detail = payload["loan_detail"]
-        user_detail = payload["user_detail"]
+        loan_detail = payload.get("loan_detail")
+        user_detail = payload.get("user_detail")
 
-        loan_amount = loan_detail["amount"]
-        loan_interest = loan_detail["interest"]
-        loan_months = loan_detail["months"]
+        loan_amount = loan_detail.get("amount")
+        loan_interest = loan_detail.get("interest")
+        loan_months = loan_detail.get("months")
 
-        user_ids = user_detail["user_ids"]
-        owner = user_detail["owner"]
+        if not loan_amount or not loan_interest or not loan_months:
+            raise Exception("missing loan details")
+        if not check_loan_details(loan_amount, loan_interest, loan_months):
+            raise Exception("invalid loan details")
 
+        user_ids = user_detail.get("user_ids")
+        owner = user_detail.get("owner")
+
+        if not user_ids or len(user_ids) == 0 or not owner:
+            raise Exception("missing user id details")
+        if not check_user_details(user_ids, owner, db_session):
+            raise Exception("invalid used detail")
 
         loan_model = models.LoanModel(amount=loan_amount, interest=loan_interest, term_months=loan_months, owner=owner)
         db_session.add(loan_model)
@@ -109,14 +167,23 @@ async def create_loan(request: Request):
         return {
             "message": "loan created",
             "data": {
-                "loan_id": loan_model.id
+                "id": loan_model.id,
+                "amount": loan_model.amount,
+                "interest": loan_model.interest,
+                "term_months": loan_model.term_months,
+                "owner": loan_model.owner
             },
             "status": 200
         }
     except Exception as e:
-        return {e}
+        return {
+            "message": str(e),
+            "data": {},
+            "status": 400
+        }
     finally:
         db_session.close()
+
 
 @app.get("/loans/{loan_id}")
 def get_loan(loan_id: int):
@@ -142,6 +209,7 @@ def get_loan(loan_id: int):
         return {e}
     finally:
         db_session.close()
+
 
 @app.get("/loans/user/{user_id}")
 def get_user_loans(user_id: int):
@@ -169,6 +237,7 @@ def get_user_loans(user_id: int):
     finally:
         db_session.close()
 
+
 @app.post("/loans/share")
 async def share_loan(request: Request):
     """
@@ -186,7 +255,7 @@ async def share_loan(request: Request):
         user_id = payload['user_id']
         loan_id = payload['loan_id']
         if not user_id or not loan_id:
-            return{
+            return {
                 "message": "invalid payload",
                 "data": {},
                 "status": 400
@@ -237,28 +306,27 @@ def get_loan_schedule(loan_id: int):
         term_months = loan_obj.term_months
         interest = loan_obj.interest
 
-        monthly_interest_rate = (interest/100)/12
-        numerator = monthly_interest_rate * math.pow((1+monthly_interest_rate), term_months)
+        monthly_interest_rate = (interest / 100) / 12
+        numerator = monthly_interest_rate * math.pow((1 + monthly_interest_rate), term_months)
         denominator = ((math.pow(1 + monthly_interest_rate, term_months)) - 1)
 
-        EMI_raw = amount * (numerator/denominator)
+        EMI_raw = amount * (numerator / denominator)
         rounded_EMI = round(EMI_raw, 2)
         rounded_EMI_cents = int(rounded_EMI * 100)
 
-
-        principal_cents = int(amount *100)
+        principal_cents = int(amount * 100)
         result_list = []
-        for month in range(1, term_months+1):
+        for month in range(1, term_months + 1):
             monthly_interest_amount_cents = int(monthly_interest_rate * principal_cents)
             total_cents = principal_cents + monthly_interest_amount_cents
             remaining_balance_cents = total_cents - rounded_EMI_cents
             if remaining_balance_cents < 0:
-                rounded_EMI = total_cents/100.00
+                rounded_EMI = total_cents / 100.00
                 remaining_balance_cents = 0
             monthly_object = {
                 "Month": month,
-                "interest": monthly_interest_amount_cents/100.00,
-                "Remaining_balance": remaining_balance_cents/100.00,
+                "interest": monthly_interest_amount_cents / 100.00,
+                "Remaining_balance": remaining_balance_cents / 100.00,
                 "Monthly_payment": rounded_EMI
             }
             result_list.append(monthly_object)
@@ -273,6 +341,7 @@ def get_loan_schedule(loan_id: int):
         return {e}
     finally:
         db_session.close()
+
 @app.get("/loans/summary/{loan_id}/month/{month_val}")
 def get_loan_summary(loan_id: int, month_val: int):
     if not loan_id or not month_val:
@@ -299,7 +368,7 @@ def get_loan_summary(loan_id: int, month_val: int):
         principal_cents = int(amount * 100)
 
         aggregate_amount_interest_paid = 0
-        for month in range(1, month_val + 1):
+        for month in range(1, month_val):
             monthly_interest_amount_cents = int(monthly_interest_rate * principal_cents)
             aggregate_amount_interest_paid += monthly_interest_amount_cents
             total_cents = principal_cents + monthly_interest_amount_cents
@@ -308,13 +377,13 @@ def get_loan_summary(loan_id: int, month_val: int):
                 rounded_EMI = total_cents / 100.00
                 remaining_balance_cents = 0
             principal_cents = remaining_balance_cents
-        aggregate_amount_principal_paid = ((amount*100) - principal_cents)/100.00
+        aggregate_amount_principal_paid = ((amount * 100) - principal_cents) / 100.00
 
         return {
             "data": {
-                "Current_Principal": principal_cents/100.00,
-                "Aggregate Amount of interest paid": aggregate_amount_interest_paid/100.00,
-                "Aggregate AMount of principal paid": aggregate_amount_principal_paid
+                "Current_Principal": principal_cents / 100.00,
+                "Aggregate Amount of interest paid": aggregate_amount_interest_paid / 100.00,
+                "Aggregate Amount of principal paid": aggregate_amount_principal_paid
             },
             "status": 200
         }
@@ -323,3 +392,43 @@ def get_loan_summary(loan_id: int, month_val: int):
         return {e}
     finally:
         db_session.close()
+
+
+def check_email(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+    # pass the regular expression
+    # and the string into the fullmatch() method
+    if (re.fullmatch(regex, email)):
+        return True
+
+    else:
+        return False
+
+
+def check_loan_details(loan_amount, loan_interest, loan_months):
+    """
+    loan amount has to be an integer value
+    loan interest has to be a float value
+    loan months has to be an integer value and can not be greater than 360 months (30 years)
+    """
+    if not isinstance(loan_amount, int):
+        return False
+    if not isinstance(loan_interest, float):
+        return False
+    if not isinstance(loan_months, int) or loan_months > 360:
+        return False
+
+    return True
+
+
+def check_user_details(user_ids, owner, db_session):
+    if not isinstance(owner, int):
+        return False
+    for id in user_ids:
+        if not isinstance(id, int):
+            return False
+        user_obj = db_session.query(models.UserModel).filter(models.UserModel.id == id).first()
+        if not user_obj:
+            return False
+    return True
